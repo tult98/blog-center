8002d9b011acb13b1c807b48d3ca1dad173d875a
Tu Le Thanh <lethanhtu1551998@gmail.com>
2023-01-10 08:25:28 +0700

    feat: add services & setup for development

---
+++ dev.py
@@ -0,0 +1,17 @@
+import fire
+from python_on_whales import docker
+
+def dev(service_name):
+#    Step 1: kill all container(s) of the service
+#    Step 2: Run a container in interactive shell
+    container_name = '_'.join(service_name.split('-'))
+    try:
+        container = docker.container.inspect(container_name)
+        if container.state.running:
+            container.remove(force=True)
+        docker.compose.run(service=service_name, name=container_name, service_ports=True, tty=True, remove=True, command=["sh"])
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+  fire.Fire(dev)
