1db99441087ca3d97cbf5f9dcf0d4d580c6785ae
Tu Thanh Le <lethanhtu1551998@gmail.com>
2023-02-26 15:07:46 +0700

    fix: update due to changes of container's name

--- dev.py
+++ dev.py
@@ -6,11 +6,10 @@ class Container():
     def dev(self, service_name):
     #    Step 1: kill all container(s) of the service
     #    Step 2: Run a container in interactive shell
-        container_name = '_'.join(service_name.split('-'))
         try:
-            if docker.container.exists(container_name):
-                docker.container.remove([container_name], force=True)
-            docker.compose.run(service=service_name, name=container_name, service_ports=True, tty=True, remove=True, command=["sh"])
+            if docker.container.exists(service_name):
+                docker.container.remove([service_name], force=True)
+            docker.compose.run(service=service_name, name=service_name, service_ports=True, tty=True, remove=True, command=["sh"])
         except Exception as e:
             print(e)
 
